6a23a72fe3da2eaded842d3dc852ed9cdd2f45a6
Виктория <mashenko.victoria@yandex.ru>
2025-06-11 20:59:49 +0500

    Add files via upload

---
+++ RequestsTest/main.py
@@ -0,0 +1,36 @@
+import requests
+
+URL = 'https://api.pokemonbattle.ru/v2'
+TOKEN = '7ad7c8447790f057c277164024909fc9'
+pokemon_id = "275895"
+HEADER = {'Content-Type' : 'application/json', 'trainer_token' : TOKEN}
+body_createpokemon = {
+    "name": "-                                                -",
+    "photo_id": 79
+}
+
+'''response_createpokemon = requests.post(url = f'{URL}/pokemons', headers = HEADER, json = body_createpokemon)
+print(response_createpokemon.status_code)
+print(response_createpokemon.text)'''
+
+'''pokemon_id = response_createpokemon.json()['id']
+print(pokemon_id)'''
+
+body_changepokemon = {
+    "pokemon_id": pokemon_id,
+    "name": "-                                                -",
+    "photo_id": 79
+}
+
+'''response_changepokemon = requests.put(url = f'{URL}/pokemons', headers = HEADER, json = body_changepokemon)
+print(response_changepokemon.status_code)
+print(response_changepokemon.text)'''
+
+body_catchpokemon = {
+    "pokemon_id": pokemon_id
+}
+
+'''response_catchpokemon = requests.post(url = f'{URL}/trainers/add_pokeball', headers = HEADER, json = body_catchpokemon)
+print(response_catchpokemon.text)'''
+
+
