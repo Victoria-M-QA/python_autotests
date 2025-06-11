1dcab6709c7992aa17c6acf0ed39e6e2c372a07a
Виктория <mashenko.victoria@yandex.ru>
2025-06-11 22:32:24 +0500

    Update main.py

--- RequestsTest/main.py
+++ RequestsTest/main.py
@@ -2,19 +2,19 @@ import requests
 
 URL = 'https://api.pokemonbattle.ru/v2'
 TOKEN = '7ad7c8447790f057c277164024909fc9'
-pokemon_id = "275895"
+#pokemon_id = "275895"
 HEADER = {'Content-Type' : 'application/json', 'trainer_token' : TOKEN}
 body_createpokemon = {
-    "name": "-                                                -",
+    "name": "Слоупок",
     "photo_id": 79
 }
 
-'''response_createpokemon = requests.post(url = f'{URL}/pokemons', headers = HEADER, json = body_createpokemon)
+response_createpokemon = requests.post(url = f'{URL}/pokemons', headers = HEADER, json = body_createpokemon)
 print(response_createpokemon.status_code)
-print(response_createpokemon.text)'''
+print(response_createpokemon.text)
 
-'''pokemon_id = response_createpokemon.json()['id']
-print(pokemon_id)'''
+pokemon_id = response_createpokemon.json()['id']
+print(pokemon_id)
 
 body_changepokemon = {
     "pokemon_id": pokemon_id,
@@ -22,15 +22,16 @@ body_changepokemon = {
     "photo_id": 79
 }
 
-'''response_changepokemon = requests.put(url = f'{URL}/pokemons', headers = HEADER, json = body_changepokemon)
+response_changepokemon = requests.put(url = f'{URL}/pokemons', headers = HEADER, json = body_changepokemon)
 print(response_changepokemon.status_code)
-print(response_changepokemon.text)'''
+print(response_changepokemon.text)
 
 body_catchpokemon = {
     "pokemon_id": pokemon_id
 }
 
-'''response_catchpokemon = requests.post(url = f'{URL}/trainers/add_pokeball', headers = HEADER, json = body_catchpokemon)
-print(response_catchpokemon.text)'''
+response_catchpokemon = requests.post(url = f'{URL}/trainers/add_pokeball', headers = HEADER, json = body_catchpokemon)
+print(response_catchpokemon.status_code)
+print(response_catchpokemon.text)
 
 
